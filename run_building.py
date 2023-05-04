ef7f5a1f9849c3b9fb996bca647bc5965c8caf89
Joern Ploennigs <Joern.Ploennigs@ie.ibm.com>
2023-05-04 16:55:03 +0200

    Update run_building.py

--- run_building.py
+++ run_building.py
@@ -1,10 +1,10 @@
 from ctgcn.causal_discovery import DecomposedCausalDiscovery, CausalDiscovery
 
-graphFixed = DecomposedCausalDiscovery('Data/Building/buildingheating.npy',
+DecomposedCausalDiscovery('Data/Building/buildingheating.npy',
                                   tau_max=6, decomp_period=144, decomp_clusters=10, reuse_clusters=True, max_steps=61,
                                   aggregation_method='ANYW',result_path='Results/buildingheating_fixed')
 
 
-graphFlex = DecomposedCausalDiscovery('Data/Building/buildingheating.npy',
+DecomposedCausalDiscovery('Data/Building/buildingheating.npy',
                                   tau_max=6, decomp_period=144, decomp_clusters=10, reuse_clusters=False, max_steps=61,
                                   aggregation_method='ANYW',result_path='Results/buildingheating_flex')
