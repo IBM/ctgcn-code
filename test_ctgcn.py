e2b1de8fd2613ddd5f2e61d0e5c134395d90194d
Joern Ploennigs <joern.ploennigs@uni-rostock.de>
2023-05-04 11:03:01 +0200

    Update test_ctgcn.py

--- test_ctgcn.py
+++ test_ctgcn.py
@@ -1,8 +1,5 @@
 from ctgcn.causal_discovery import DecomposedCausalDiscovery, CausalDiscovery
-import numpy as np
 
-graph = DecomposedCausalDiscovery('/Users/abilangbridge/Desktop/IBM/AI4DT/CTGCN/Data/Building/buildingheating.npy',
-                                  tau_max=6, decomp_period=144, decomp_clusters=3, cluster_seeding=True,
+graph = DecomposedCausalDiscovery('Data/Building/buildingheating.npy',
+                                  tau_max=6, decomp_period=144, decomp_clusters=3, reuse_clusters=True,
                                   aggregation_method='ANYW')
-
-np.save(f"/Users/abilangbridge/Desktop/IBM/AI4DT/CTGCN/Graphs/Building/T6_PT144_C3_Seeding_ANYW.npy", graph)
