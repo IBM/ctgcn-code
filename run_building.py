9f7a75356cb05ad0c711f41c793e61516025d8e6
Joern Ploennigs <joern.ploennigs@uni-rostock.de>
2023-05-05 13:19:28 +0200

    Update run_building.py

--- run_building.py
+++ run_building.py
@@ -2,13 +2,17 @@ from ctgcn.causal_discovery import DecomposedCausalDiscovery, CausalDiscovery
 
 import gpytorch
 
-with gpytorch.settings.cholesky_jitter(1e-1) and gpytorch.settings.verbose_linalg(True) and gpytorch.settings.fast_computations(True,True,True):
-    DecomposedCausalDiscovery('Data/Building/buildingheating.npy',
+
+
+with gpytorch.settings.fast_computations(True,True,True):
+    with gpytorch.settings.cholesky_jitter(1):
+        DecomposedCausalDiscovery('Data/Building/buildingheating.npy',
                                   tau_max=6, decomp_period=144, decomp_clusters=10, reuse_clusters=True, max_steps=61, ci_test='GPDCTorch',
                                   aggregation_method='ANYW',result_path='Results/buildingheating_fixed_GPDCTorch')
 
 
-with gpytorch.settings.cholesky_jitter(1e-1) and gpytorch.settings.verbose_linalg(True) and gpytorch.settings.fast_computations(True,True,True):
-    DecomposedCausalDiscovery('Data/Building/buildingheating.npy',
+with gpytorch.settings.fast_computations(True,True,True):
+    with gpytorch.settings.cholesky_jitter(1):
+        DecomposedCausalDiscovery('Data/Building/buildingheating.npy',
                                   tau_max=6, decomp_period=144, decomp_clusters=10, reuse_clusters=False, max_steps=61, ci_test='GPDCTorch',
                                   aggregation_method='ANYW',result_path='Results/buildingheating_flex_GPDCTorch')
