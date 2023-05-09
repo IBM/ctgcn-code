d221c86d7dea1b47b35ebf92f65392c4a9f7880f
Joern Ploennigs <Joern.Ploennigs@ie.ibm.com>
2023-05-09 15:15:30 +0200

    Update run_building.py

--- run_building.py
+++ run_building.py
@@ -3,16 +3,21 @@ from ctgcn.causal_discovery import DecomposedCausalDiscovery, CausalDiscovery
 import gpytorch
 
 
+with gpytorch.settings.fast_computations(True,True,True):
+    with gpytorch.settings.cholesky_jitter(1):
+        DecomposedCausalDiscovery('Data/Building/buildingheating.npy',
+                                  tau_max=6, decomp_period=144, decomp_clusters=10, one_cluster=True, ci_test='ParCorr',
+                                  aggregation_method='ANYW',result_path='Results/buildingheating_single2')
 
 with gpytorch.settings.fast_computations(True,True,True):
     with gpytorch.settings.cholesky_jitter(1):
         DecomposedCausalDiscovery('Data/Building/buildingheating.npy',
-                                  tau_max=6, decomp_period=72, decomp_clusters=10, reuse_clusters=True, max_steps=61, ci_test='GPDCTorch',
-                                  aggregation_method='ANYW',result_path='Results/buildingheating_fixed_GPDCTorch')
+                                  tau_max=6, decomp_period=144, decomp_clusters=10, reuse_clusters=True, ci_test='ParCorr',
+                                  aggregation_method='ANYW',result_path='Results/buildingheating_fixed2')
 
 
 with gpytorch.settings.fast_computations(True,True,True):
     with gpytorch.settings.cholesky_jitter(1):
         DecomposedCausalDiscovery('Data/Building/buildingheating.npy',
-                                  tau_max=6, decomp_period=144, decomp_clusters=10, reuse_clusters=False, max_steps=61, ci_test='GPDCTorch',
-                                  aggregation_method='ANYW',result_path='Results/buildingheating_flex_GPDCTorch')
+                                  tau_max=6, decomp_period=144, decomp_clusters=10, reuse_clusters=False, ci_test='ParCorr',
+                                  aggregation_method='ANYW',result_path='Results/buildingheating_flex2')
