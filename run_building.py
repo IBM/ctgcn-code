d9deb406876aa88c6946ecd527905022551c8914
Joern Ploennigs <joern.ploennigs@uni-rostock.de>
2023-05-15 09:26:44 +0200

    Update run_building.py

--- run_building.py
+++ run_building.py
@@ -2,11 +2,11 @@ from ctgcn.causal_discovery import DecomposedCausalDiscovery, CausalDiscovery
 import gpytorch
 
 
-with gpytorch.settings.fast_computations(True,True,True):
-    with gpytorch.settings.cholesky_jitter(1):
-        DecomposedCausalDiscovery('Data/Building/buildingheating.npy',
-                                  tau_max=6, decomp_period=144, decomp_clusters=10, one_cluster=True, ci_test='ParCorr',
-                                  aggregation_method='ANYW',result_path='Results/buildingheating_single10')
+#with gpytorch.settings.fast_computations(True,True,True):
+#    with gpytorch.settings.cholesky_jitter(1):
+#        DecomposedCausalDiscovery('Data/Building/buildingheating.npy',
+#                                  tau_max=6, decomp_period=144, decomp_clusters=10, one_cluster=True, ci_test='ParCorr',
+#                                  aggregation_method='ANYW',result_path='Results/buildingheating_single10')
 
 
 with gpytorch.settings.fast_computations(True,True,True):
